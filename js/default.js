9d5537267740fd98db565bb3246bdc42342123d5
kougei1527050 <g1527050@st.t-kougei.ac.jp>
2016-06-20 17:21:53 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,2 +1,17 @@
-window.alert('こんにちは');
-console.log('こんにちは！')
+//こたえ
+var nunber=Math.floor(Math.random()*3);
+var answer=parseInt(window.prompt('数あてゲーム。0～2の数字を入力してください。'));
+
+var message;
+if(answer===number){
+  message='あたりー！';
+}else if(answer<nunber){
+  message='もう少し大きいよ';
+}else if(nunber<answer){
+  message='もうすこし小さいよ';
+}else{
+  message='0～2のの数字を入力してください';
+}
+
+//結果表示
+document.getElementById('choice').textContent=message;
